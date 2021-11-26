2197030a553e5c08109ba4cea38dfa59590401c6
rka <russkikh_k@ekb.sima-land.ru>
2021-11-27 00:00:42 +0500

    +

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,13 @@ import Read from './reader';
 import json from './parser';
 
 export default class GameSavingLoader {
-  static load() {
-    return (async () => {
-      try {
-        const response = await Read();
-        const data = await json(response);
-        return data;
-      } catch (err) {
-        return err;
-      }
-    })();
+  static async load() {
+    try {
+      const response = await Read();
+      const data = await json(response);
+      return data;
+    } catch (err) {
+      return err;
+    }
   }
 }
